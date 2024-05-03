023b3f760acdec86f432f3d327e6816e7be3cb46
Matheus Silva <mate.almeidasilva@gmail.com>
2024-05-03 18:07:21 -0300

    creating project and install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html, js}"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        "home": "url('/assets/img/bg.png')"
+      }
+    },
+  },
+  plugins: [],
+}
+
+ 
